c3b5f81ae8394eed8a68acbf7d7c1361499bb559
Satya <satyanaidugandrothu@gmail.com>
2023-03-29 13:41:20 +0530

    Second commit

--- FirstGit.java
+++ FirstGit.java
@@ -2,5 +2,6 @@ public class FirstGit{
     public static void main(String[] args) {
         System.out.println("Welcome to Git");
         System.out.println("Hii This is Git");
+        System.out.println("Hello");
     }
 }
